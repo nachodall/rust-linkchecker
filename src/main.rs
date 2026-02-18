6e93e4fe5074ba3fbeacefb52966d8a037dcd964
nachodall <ignaciodallacqua@gmail.com>
2026-02-18 01:11:34 -0300

    feat: basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Args {
+    input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let content = fs::read_to_string(&args.input).expect("Program could not read the input file");
+
+    println!("File loaded successfully: {:?}", args.input);
+    println!("Contenido: {} bytes", content.len());
 }
