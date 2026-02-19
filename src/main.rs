ee7d4f0e9fd2914b844fbc117c316b907987450b
nachodall <ignaciodallacqua@gmail.com>
2026-02-19 18:10:57 -0300

    feat: implement concurrent link checking using JoinSet

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
+use tokio::task::JoinSet;
 
 mod client;
 mod models;
@@ -15,16 +16,26 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let _ = parser::extract_title(""); //Making clippy happy
+    let _ = parser::extract_title("");
 
     let args = Args::parse();
     let content = fs::read_to_string(&args.input)?;
     let links = parser::extract_links(&content);
     let http_client = reqwest::Client::new();
 
+    let mut set = JoinSet::new();
+
     for link in links {
-        let mut res = models::LinkCheckResult::new(link);
-        client::check_url(&http_client, &mut res).await;
+        let client = http_client.clone();
+        set.spawn(async move {
+            let mut res = models::LinkCheckResult::new(link);
+            client::check_url(&client, &mut res).await;
+            res
+        });
+    }
+
+    while let Some(res) = set.join_next().await {
+        let res = res?;
         println!("{}", res.produce_link_checker_report());
     }
 
